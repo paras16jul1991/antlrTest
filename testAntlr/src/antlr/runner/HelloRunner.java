8c07641b970f20f267c9e6c2d6f10a80a00a67cb
paras <paras16jul1991@gmail.com>
2017-03-28 23:08:34 +0530

    Initial code added.

---
+++ testAntlr/src/antlr/runner/HelloRunner.java
@@ -0,0 +1,22 @@
+package antlr.runner;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+import antlr4.HelloLexer;
+import antlr4.HelloParser;
+
+public class HelloRunner {
+	public static void main(String[] args) throws Exception {
+
+		ANTLRInputStream input = new ANTLRInputStream(System.in);
+
+		HelloLexer lexer = new HelloLexer(input);
+
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+		HelloParser parser = new HelloParser(tokens);
+		ParseTree tree = parser.r(); // begin parsing at rule 'r'
+		System.out.println(tree.toStringTree(parser)); // print LISP-style tree
+	}
+
+}
